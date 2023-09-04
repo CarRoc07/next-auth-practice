1daf417c1f400add2573961b58a4128f43612b5d
CarRoc07 <carlovhd@gmail.com>
2023-09-04 11:30:10 -0300

    add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className='p-4 text-xl'>
-      HomePage
+    <div className='p-4 text-2xl flex flex-col items-center'>
+      <h2>This is the Home Page!</h2>
+      <h3> Please if you are not logged in, log in </h3>
     </div>
   )
 }
--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -1,8 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
 import React from 'react'
+import { useSession } from 'next-auth/react'
 
 const PanelPage = () => {
+    const { data: session } = useSession();
+
     return (
-        <div className='p-4 text-xl'>Panel Page</div>
+        <div className='p-4 text-xl flex flex-col items-center gap-5'>
+            <h2>This is a protected route, you can see your private data here!</h2>
+            <div className='flex flex-col items-center gap-6'>
+                <img src={session?.user?.image ? session.user.image : ''} className='rounded-full cursor-pointer hover:opacity-50' alt='Profile image' />
+                <input type="text" value={session?.user?.name ? session.user.name : 'Your name'} className='bg-gray-300 text-black p-3 rounded-lg' />
+                <input type="text" value={session?.user?.email ? session.user.email : 'Your email'} className='bg-gray-300 text-black p-3 rounded-lg' />
+            </div>
+        </div>
     )
 }
 
